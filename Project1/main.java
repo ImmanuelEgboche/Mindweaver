d16fd80b26fe3035f81b86bcbd9a0aa96e9c9703
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-09-05 20:13:49 +0100

    figuring out the difference between quotations :)

--- Project1/main.java
+++ Project1/main.java
@@ -1,7 +1,16 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
+    
+    
+    // String num2 = '2';
+    // String num3 = '3';
+    // String num4 = '4';
+    
+
+
     static void createFile(){
         try{
             File obj = new File("newfile.txt");
@@ -25,8 +34,46 @@ public class main {
             }
     }
     public static void main(String[] args){
-        System.out.println("Welcome to *insert name*");
-        deleteFile();
+        String num1 = "1";
+        // String s = String.valueOf(num1);
+        System.out.println(num1);
+        System.out.println("Welcome");
+        Scanner obj = new Scanner(System.in); // creates scanner object 
+        System.out.println("Enter a option");
+        String input = obj.nextLine();
+        System.out.println(input.equals("1")); 
+           // not true so while loop and if statement not triggering 
+        while(true){
+            if(input.equals("1")){
+                System.out.println("Do you want to add or create a file");
+                Scanner menu1 = new Scanner(System.in); // creates scanner object 
+                String men = menu1.nextLine();
+                if(men.equals("create")){
+                    createFile();
+                }
+                break;
+            }
+            /
+        }
+        
+        // if(input == num1){
+        //         System.out.println("this is 1");
+                
+        //  }
+        // if(input == "2"){
+        //         System.out.println("this is 2 ");
+                
+        //  }
+        // if(input == "3"){
+        //         System.out.println("this is 3");
+                
+        //  }
+        // if(input == "4"){
+        //         System.out.println("this is 4");
+                
+        //  } else {
+        //      System.out.println("Not a vaild option");
+        //  }
         
     }
 }
