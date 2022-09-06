96a21c48c2561d0be6be8601f4d752662e9d8992
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-09-06 14:07:47 +0100

    fixing while true statement for menu

--- Project1/main.java
+++ Project1/main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main {
     
@@ -9,13 +10,24 @@ public class main {
     // String num3 = '3';
     // String num4 = '4';
     
-
+    static void addArray(String name){
+        ArrayList<String> items = new ArrayList<String>();
+        items.add(name);
+        System.out.println(items);
+        
+    }
 
     static void createFile(){
         try{
-            File obj = new File("newfile.txt");
-            if(obj.createNewFile()){
-            System.out.println("file created " + obj.getName());
+            Scanner obj = new Scanner(System.in); // creates scanner object 
+            System.out.println("Enter FILE NAME");
+            String input = obj.nextLine();
+            String string = String.format(" %s.txt", input);
+            addArray(string);
+            System.out.println(string);
+            File fobj = new File(string);
+            if(fobj.createNewFile()){
+            System.out.println("file created " + fobj.getName());
             } else {
                 System.out.println("file already exists");
         }
@@ -34,26 +46,64 @@ public class main {
             }
     }
     public static void main(String[] args){
-        String num1 = "1";
         // String s = String.valueOf(num1);
-        System.out.println(num1);
-        System.out.println("Welcome");
         Scanner obj = new Scanner(System.in); // creates scanner object 
-        System.out.println("Enter a option");
+        System.out.println("Welcome \n 1 to create or add \n 2 for deleting \n 3 to find a file \n 0 to exit");
         String input = obj.nextLine();
         System.out.println(input.equals("1")); 
            // not true so while loop and if statement not triggering 
         while(true){
+            
+
+            
             if(input.equals("1")){
+                
+
+                
                 System.out.println("Do you want to add or create a file");
                 Scanner menu1 = new Scanner(System.in); // creates scanner object 
                 String men = menu1.nextLine();
+                while(true){
+                    
                 if(men.equals("create")){
                     createFile();
                 }
+                if (men.equals("add")){
+                    /*addFile(); 
+                    needs to be implemented*/
+                    System.out.println("add if");
+                }
+                if (men.equals("none")){
+                    System.out.println("bye");
+                    /*addFile(); 
+                    needs to be implemented*/
+                    
+                } 
+                    break;
+                }
+                break;
+            } 
+            if(input.equals("2")){
+                System.out.println("Do you want to delete a file");
+                Scanner menu1 = new Scanner(System.in); // creates scanner object 
+                String del = menu1.nextLine();
+                if(del.equals("create")){ // delete method needs to take argument 
+                    deleteFile();
+                }
                 break;
             }
-            /
+            if(input.equals("3")){
+                System.out.println("What file are you looking for?");
+                /* 
+                    Need a sorting algorthim then a searching algorthim 
+
+                    BINARY SEARCH FOR SEARCHING 
+
+                    SORTING QUICK SORT 
+
+                    NEED TO ACCESS FILE PATH THEN OPEN FILE AFTER SEARCHING 
+                */
+            }
         }
         
         // if(input == num1){
